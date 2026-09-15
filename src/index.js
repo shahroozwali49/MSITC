2f100b9df2425738332aeca97c0ffff7fa403fee
shahroozwali49 <shahroozwali49@gmail.com>
2026-09-15 17:03:05 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MoonStarPOS from './App';
+import './index.css';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MoonStarPOS />
+  </React.StrictMode>
+);
